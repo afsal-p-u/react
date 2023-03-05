70f1031dae12f7cfaa7eac1ecc57eb72cdd654de
Afsal P U <afsalpu16@gmail.com>
2023-03-06 00:00:35 +0530

    119

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,17 @@
 import {Outlet, Link} from "react-router-dom";
 import { Fragment, useContext } from "react";
-
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import { UserContext } from "../../contexts/user.context";
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { CartContext } from "../../contexts/cart.context";
 import './navigation.styles.scss'
 
 
 const Navigation = () =>{
-  const { currentUser } = useContext(UserContext)
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
     return(
       <Fragment>
@@ -27,7 +30,9 @@ const Navigation = () =>{
                 SIGN IN
             </Link>
             )}
+            <CartIcon />
           </div>
+          {isCartOpen && <CartDropdown />} 
         </div>
         <Outlet />
       </Fragment>
